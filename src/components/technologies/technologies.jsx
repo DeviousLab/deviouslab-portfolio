da388cbf76dd455958a9f584394dffc164be6cc1
DeviousLab <deviouslab@gmail.com>
2021-12-05 00:01:21 +0400

    Basic technologies section setup
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/components/technologies/technologies.jsx
+++ src/components/technologies/technologies.jsx
@@ -4,9 +4,50 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './technologies.styles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id='tech'>
+    <SectionDivider />
+    <SectionTitle>
+      Technologies
+    </SectionTitle>
+    <SectionText>
+      I have experience with the following technologies:
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiFirebase size={40} />
+        <ListContainer>
+          <ListTitle>
+            Firebase
+          </ListTitle>
+          <ListParagraph>
+            Firebase is a cloud-based platform for hosting and managing web apps.
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size={40} />
+        <ListContainer>
+          <ListTitle>
+            Firebase
+          </ListTitle>
+          <ListParagraph>
+            Firebase is a cloud-based platform for hosting and managing web apps.
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size={40} />
+        <ListContainer>
+          <ListTitle>
+            Firebase
+          </ListTitle>
+          <ListParagraph>
+            Firebase is a cloud-based platform for hosting and managing web apps.
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
