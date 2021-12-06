ed025e0cc7368b521dd3c9a4b1af229bbd90729b
DeviousLab <deviouslab@gmail.com>
2021-12-06 23:48:00 +0400

    Reorganised layout of sections on page
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,9 +14,9 @@ const Home = () => {
         <Hero />
         <BackgroundAnimation />
       </Section>
-      <Projects />
-      <Technologies />
       <Timeline />
+      <Technologies />
+      <Projects />
       <Acomplishments />
     </Layout>
   );
