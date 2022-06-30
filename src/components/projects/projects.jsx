439409a9174b4cfea2aa383d570c9e2209e05e51
DeviousLab <deviouslab@gmail.com>
2022-06-30 21:21:31 +0400

    Removed unnecessary component
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => (
         <BlogCard key={id}>
           <Img src={image} alt={title} />
           <TitleContent>
-            <HeaderThree title>{title}</HeaderThree>
+            <HeaderThree title="true">{title}</HeaderThree>
             <Hr />
           </TitleContent>
           <CardInfo>
