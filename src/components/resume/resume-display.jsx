508141224239bb745c206f93d7cdd9fdbfad7177
DeviousLab <deviouslab@gmail.com>
2022-07-01 05:02:41 +0400

    Completed styling the resume component
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/components/resume/resume-display.jsx
+++ src/components/resume/resume-display.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
+import { HiDocumentDownload } from 'react-icons/hi';
+import Link from 'next/link';
 
 import { Section } from '../../styles/global-components/app.styles';
-import { ResumeContainer } from './resume-display.styles';
+import { ResumeWrapper, ResumeContainer, ResumeBottom } from './resume-display.styles';
 
 export default function ResumeDisplay() {
   return (
-    <Section>
-      <ResumeContainer>
-      <Document file={'/pdfs/HimeshResume.pdf'}>
-        <Page pageNumber={1} />
-      </Document>
-      </ResumeContainer>
+    <Section style={{ overflow: 'visible' }}>
+      <ResumeWrapper>
+        <ResumeContainer>
+          <Document file={'/pdfs/HimeshResume.pdf'}>
+            <Page pageNumber={1} />
+          </Document>
+        </ResumeContainer>
+        <Link href='/pdfs/HimeshResume.pdf'>
+          <ResumeBottom>
+            <span>Download</span> <HiDocumentDownload />
+          </ResumeBottom>
+        </Link>
+      </ResumeWrapper>
     </Section>
   )
 }
--- src/components/resume/resume-display.styles.jsx
+++ src/components/resume/resume-display.styles.jsx
@@ -1,15 +1,55 @@
 import styled from 'styled-components';
 
-
-export const ResumeContainer = styled.div`
-  border-radius: 10px;
-  box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
-  text-align: center;
-  width: 100%;
+export const ResumeWrapper = styled.div`
+  background: white;
+  margin: auto;
+  position: relative;
+  border-radius: 5px;
+  box-shadow: 0px 0px 15px 20px rgba(238,238,238,0.3);
   @media ${(props) => props.theme.breakpoints.sm} {
-    width: 100%;
+    width: 80%;
   }
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+export const ResumeContainer = styled.div`
+  width:100%;
+  height:100%;
+  border-radius: 5px;
+  -webkit-background-size: 100%;
+  -moz-background-size: 100%;
+  -o-background-size: 100%;
+  background-size: 100%;
+`;
+
+export const ResumeBottom = styled.div`
+  transition: background 0.5s;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  background: #023e8a;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+  cursor: pointer;
+  >svg {
+        transition: transform 0.5s;
+      }
+      &:hover{
+        background: #13ADC7;
+      }
+    }
+    &:hover >svg {
+      transform: translateY(2px);
+    }
+  >span {
+    transition: transform 0.5s;
+  }
+    &:hover >span {
+      transform: translateY(2px);
+    }
 `;
